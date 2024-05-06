24afafd1028e12d895f46a06fb59d40a9437b18f
Harry Richmond <rogerbytes@vivaldi.net>
2024-05-06 20:11:29 +0200

    no comment

--- script/main.js
+++ script/main.js
@@ -7,19 +7,50 @@
 
 const device_type = detect_device_type().toString(),
   bean_box = document.getElementById("beanBox"),
-  scroll_bean = document.getElementById("scrollBean"),
+  scroll_bean = document.querySelector('#scrollBean'),
   main_box = document.getElementById("mainBox"),
   main_box_height = main_box.scrollHeight,
   main_box_visible_height = main_box.clientHeight,
   main_box_overflow = main_box.scrollHeight - main_box.clientHeight,
-  overflowQuotient = main_box.scrollHeight / main_box_visible_height;
+  overflow_quotient = main_box.scrollHeight / main_box_visible_height;
+
 
 //!-------------  Déclaration des Events  ------------------//
+
+// Écouteur d'événements pour le défilement
+main_box.addEventListener('scroll', function() {
+  var scrollPosition = main_box.scrollTop;
+  var scrollBeanPosition = (scrollPosition / main_box_overflow) * 100;
+  scroll_bean.style.top = scrollBeanPosition + '%';
+});
+
+// Écouteurs pour les événements 'mousedown' et 'touchstart' sur 'scroll_bean'
+scroll_bean.addEventListener('mousedown', function(e) {
+  // Empêcher le comportement par défaut de sélection de texte, etc.
+  e.preventDefault();
+  document.addEventListener('mousemove', moveScrollBean);
+  document.addEventListener('mouseup', function() {
+    document.removeEventListener('mousemove', moveScrollBean);
+  });
+});
+
+scroll_bean.addEventListener('touchstart', function(e) {
+  e.preventDefault();
+  document.addEventListener('touchmove', function(ev) {
+    moveScrollBean(ev.touches[0]);
+  });
+  document.addEventListener('touchend', function() {
+    document.removeEventListener('touchmove', moveScrollBean);
+  });
+});
 
 //!-------------  Instructions  ----------------------------//
 
 document.body.classList.add(device_type.toLowerCase().replace(" ", "-"));
 show();
+
+scroll_bean.style.height = `${100 / overflow_quotient}%`;
+
 
 //?-------------  Déclaration des Fonctions  ---------------//
 
@@ -40,8 +71,17 @@ function detect_device_type() {
 }
 
 function show() {
-  console.log(`L'overflow dépasse de ${overflowQuotient * 100}%`);
+  console.log(`L'overflow dépasse de ${100 / overflow_quotient}% de quotient`);
 }
+
+// Fonction pour déplacer 'scroll_bean' et faire défiler 'main_box'
+function moveScrollBean(e) {
+  var beanBoxRect = bean_box.getBoundingClientRect();
+  var newBeanPosition = (e.clientY - beanBoxRect.top) / beanBoxRect.height;
+  scroll_bean.style.top = (newBeanPosition * 100) + '%';
+  main_box.scrollTop = newBeanPosition * main_box_overflow;
+}
+
 
 //todo----------  TODO  ------------------------------------//
 
