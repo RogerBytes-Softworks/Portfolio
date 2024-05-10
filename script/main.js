b7a7aa00a6fc86ee6be826ff54bdf36e62a889c1
Harry Richmond <rogerbytes@vivaldi.net>
2024-05-10 16:54:16 +0200

    no comment

--- script/main.js
+++ script/main.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 //?-------------  Déclaration des Imports  -----------------//
 
 
@@ -8,139 +7,154 @@
 
 const device_type = detect_device_type().toString(),
   bean_box = document.getElementById("beanBox"),
-  scroll_bean = document.querySelector('#scrollBean'),
+  scroll_bean = document.querySelector("#scrollBean"),
   main_box = document.getElementById("mainBox"),
   main_box_height = main_box.scrollHeight,
   main_box_visible_height = main_box.clientHeight,
   main_box_overflow = main_box.scrollHeight - main_box.clientHeight,
-  overflow_quotient = main_box.scrollHeight / main_box_visible_height;
+  overflow_quotient = main_box.scrollHeight / main_box_visible_height,
+  style = document.createElement("style");
 
-var label_menu = document.getElementById('labelMenu');
+var label_menu = document.getElementById("labelMenu");
 
+document.head.appendChild(style);
 
 //!-------------  Déclaration des Events  ------------------//
 
-const style = document.createElement('style');
-document.head.appendChild(style);
-scroll_bean.onmousedown = function(event) {
+
+scroll_bean.onmousedown = function (event) {
   event.preventDefault(); // Prévenir le comportement par défaut de la sélection
   const rect = bean_box.getBoundingClientRect();
 
   // Calculer l'offset initial
-  let offsetY = event.clientY - scroll_bean.getBoundingClientRect().top;
+  let offset_y = event.clientY - scroll_bean.getBoundingClientRect().top;
 
   // Fonction pour le mouvement de glissement
-  function onMouseMove(event) {
+  function on_mouse_move(event) {
     // Calculer la nouvelle coordonnée Y
-    let new_top = event.clientY - rect.top - offsetY;
+    let new_top = event.clientY - rect.top - offset_y;
 
     // Restreindre le mouvement de scrollBean verticalement à l'intérieur de beanBox
-    new_top = Math.max(0, Math.min(new_top, rect.height - scroll_bean.offsetHeight - 4));
-
+    new_top = Math.max(
+      0,
+      Math.min(new_top, rect.height - scroll_bean.offsetHeight - 4)
+    );
 
     // Appliquer la nouvelle position Y sans changer la position X
-    scroll_bean.style.top = new_top + 'px';
+    scroll_bean.style.top = new_top + "px";
 
-    scrollToPercentage(main_box, scroll_bean_position())
+    scroll_to_percentage(main_box, scroll_bean_position());
   }
 
   // Ajouter les événements pour le mouvement et le relâchement de la souris
-  document.addEventListener('mousemove', onMouseMove);
-  document.onmouseup = function() {
-    document.removeEventListener('mousemove', onMouseMove);
+  document.addEventListener("mousemove", on_mouse_move);
+  document.onmouseup = function () {
+    document.removeEventListener("mousemove", on_mouse_move);
     scroll_bean.onmouseup = null;
   };
 };
 
-scroll_bean.ondragstart = function() {
+scroll_bean.ondragstart = function () {
   return false;
 };
 
-main_box.addEventListener('scroll', function() {
-  const scrollPercentage = calculateScrollPercentage(); // Utilisez votre fonction existante
+main_box.addEventListener("scroll", function () {
+  const scroll_percentage = calculate_scroll_percentage(); // Utilisez votre fonction existante
   const movable_height = bean_box.clientHeight - scroll_bean.offsetHeight;
-  scroll_bean.style.top = (movable_height * scrollPercentage / 100) + 'px';
+  scroll_bean.style.top = (movable_height * scroll_percentage) / 100 + "px";
 });
 
-window.addEventListener('resize', adjustBeanBoxVisibility);
+window.addEventListener("resize", adjust_bean_box_visibility);
 
 // Exécutez immédiatement la fonction pour mettre en place l'état initial correct
-window.dispatchEvent(new Event('resize'));
+window.dispatchEvent(new Event("resize"));
 
-scroll_bean.addEventListener('touchstart', function(event) {
-  event.preventDefault();
-  const rect = bean_box.getBoundingClientRect();
-  
-  // Utilisez le premier contact pour l'offset initial
-  const touch = event.touches[0];
-  let offsetY = touch.clientY - scroll_bean.getBoundingClientRect().top;
-  
-  function onTouchMove(event) {
-    // Utilisez le premier contact pour la nouvelle position Y
+scroll_bean.addEventListener(
+  "touchstart",
+  function (event) {
+    event.preventDefault();
+    const rect = bean_box.getBoundingClientRect();
+
+    // Utilisez le premier contact pour l'offset initial
     const touch = event.touches[0];
-    let new_top = touch.clientY - rect.top - offsetY;
-  
-    // Restreindre le mouvement de scroll_bean verticalement à l'intérieur de bean_box
-    new_top = Math.max(0, Math.min(new_top, rect.height - scroll_bean.offsetHeight - 4));
-  
-    // Appliquer la nouvelle position Y sans changer la position X
-    scroll_bean.style.top = new_top + 'px';
-  
-    scrollToPercentage(main_box, scroll_bean_position())
-  }
-  
-  // Ajouter les événements pour le mouvement et le relâchement du doigt
-  document.addEventListener('touchmove', onTouchMove);
-  document.addEventListener('touchend', function() {
-    document.removeEventListener('touchmove', onTouchMove);
-  });
-}, { passive: false });
+    let offset_y = touch.clientY - scroll_bean.getBoundingClientRect().top;
+
+    function on_touch_move(event) {
+      // Utilisez le premier contact pour la nouvelle position Y
+      const touch = event.touches[0];
+      let new_top = touch.clientY - rect.top - offset_y;
+
+      // Restreindre le mouvement de scroll_bean verticalement à l'intérieur de bean_box
+      new_top = Math.max(
+        0,
+        Math.min(new_top, rect.height - scroll_bean.offsetHeight - 4)
+      );
+
+      // Appliquer la nouvelle position Y sans changer la position X
+      scroll_bean.style.top = new_top + "px";
+
+      scroll_to_percentage(main_box, scroll_bean_position());
+    }
+
+    // Ajouter les événements pour le mouvement et le relâchement du doigt
+    document.addEventListener("touchmove", on_touch_move);
+    document.addEventListener("touchend", function () {
+      document.removeEventListener("touchmove", on_touch_move);
+    });
+  },
+  { passive: false }
+);
 
 // Empêchez le navigateur de réagir à des gestes de défilement/drag sur #scrollBean
-scroll_bean.addEventListener('touchmove', function(event) {
-  event.preventDefault();
-}, { passive: false });
+scroll_bean.addEventListener(
+  "touchmove",
+  function (event) {
+    event.preventDefault();
+  },
+  { passive: false }
+);
 
 //!-------------  Instructions  ----------------------------//
 
 document.body.classList.add(device_type.toLowerCase().replace(" ", "-"));
 scroll_bean.style.height = `${100 / overflow_quotient}%`;
-document.addEventListener('DOMContentLoaded', adjustBeanBoxVisibility);
+document.addEventListener("DOMContentLoaded", adjust_bean_box_visibility);
 
-const scrollPercentage = calculateScrollPercentage(); // Calculez le pourcentage de défilement actuel
+const scroll_percentage = calculate_scroll_percentage(); // Calculez le pourcentage de défilement actuel
 const movable_height = bean_box.clientHeight - scroll_bean.offsetHeight;
-scroll_bean.style.top = (movable_height * scrollPercentage / 100) + 'px';
+scroll_bean.style.top = (movable_height * scroll_percentage) / 100 + "px";
 
 if (main_box.scrollHeight <= main_box.clientHeight) {
   // S'il n'y a pas d'overflow, cachez bean_box
-  bean_box.style.display = 'none';
+  bean_box.style.display = "none";
 } else {
   // S'il y a de l'overflow, assurez-vous que bean_box est visible
-  bean_box.style.display = 'block';
+  bean_box.style.display = "block";
 }
 
-label_menu.textContent = 'Acceuil';
+label_menu.textContent = "Accueil";
 
-
-
-style.sheet.insertRule(`
+style.sheet.insertRule(
+  `
   #mainBox::-webkit-scrollbar {
     width: 0px;
     height: 0px;
   }
-`, style.sheet.cssRules.length);
+`,
+  style.sheet.cssRules.length
+);
 
 //?-------------  Déclaration des Fonctions  ---------------//
 
 function detect_device_type() {
-  const userAgent = navigator.userAgent;
+  const user_agent = navigator.userAgent;
   if (
-    userAgent.match(/Macintosh|Windows|Linux|Gecko/i) &&
-    !userAgent.match(/iPhone|iPad|iPod|Windows Phone|Android/i)
+    user_agent.match(/Macintosh|Windows|Linux|Gecko/i) &&
+    !user_agent.match(/iPhone|iPad|iPod|Windows Phone|Android/i)
   ) {
     return `desktop`;
   } else if (
-    userAgent.match(/iPhone|iPad|iPod|Windows Phone|Android|AppleWebKit/i)
+    user_agent.match(/iPhone|iPad|iPod|Windows Phone|Android|AppleWebKit/i)
   ) {
     return `mobile`;
   } else {
@@ -164,51 +178,57 @@ function scroll_bean_position() {
   return Math.min(Math.max(position_percentage, 0), 100);
 }
 
-function calculateScrollPercentage() {
-  const scrollTop = main_box.scrollTop;
-  const scrollHeight = main_box.scrollHeight;
+function calculate_scroll_percentage() {
+  const scroll_top = main_box.scrollTop;
+  const scroll_height = main_box.scrollHeight;
   const clientHeight = main_box.clientHeight;
-  
+
   // Le pourcentage de défilement est le rapport entre ce qui a déjà défilé et ce qui est défilable
-  const scrollPercentage = (scrollTop / (scrollHeight - clientHeight)) * 100;
+  const scroll_percentage = (scroll_top / (scroll_height - clientHeight)) * 100;
 
-  return scrollPercentage.toFixed(2); // Arrondi à deux décimales pour la propreté
+  return scroll_percentage.toFixed(2); // Arrondi à deux décimales pour la propreté
 }
 
-function scrollToPercentage(mainBox, percentage) {
-  const maxScrollTop = mainBox.scrollHeight - mainBox.clientHeight; // Max scrollTop value
-  const scrollTop = (percentage / 100) * maxScrollTop; // Calcul de la position de défilement en pixels
-  mainBox.scrollTop = scrollTop; // Fait défiler mainBox à la position calculée
+function scroll_to_percentage(mainBoxVar, percentage) {
+  const max_scroll_top = mainBoxVar.scrollHeight - mainBoxVar.clientHeight; // Max scroll_top value
+  const scroll_top = (percentage / 100) * max_scroll_top; // Calcul de la position de défilement en pixels
+  mainBoxVar.scrollTop = scroll_top; // Fait défiler mainBox à la position calculée
 }
 
-function adjustBeanBoxVisibility() {
+function adjust_bean_box_visibility() {
   const has_overflow = main_box.scrollHeight > main_box.clientHeight;
   const is_portrait = window.innerHeight > window.innerWidth;
 
   // Mettre à jour l'affichage de bean_box
   if (has_overflow && !is_portrait) {
-    bean_box.style.display = 'block';
+    bean_box.style.display = "block";
   } else {
-    bean_box.style.display = 'none';
+    bean_box.style.display = "none";
   }
 
   // Mettre à jour la visibilité de la barre de défilement native de mainBox
   if (is_portrait) {
     // En mode portrait, affichez la barre de défilement native
-    style.sheet.insertRule(`
+    style.sheet.insertRule(
+      `
       #mainBox::-webkit-scrollbar {
         width: 5px;
         height: 5px;
       }
-    `, style.sheet.cssRules.length);
+    `,
+      style.sheet.cssRules.length
+    );
   } else {
     // En mode paysage, cachez la barre de défilement native
-    style.sheet.insertRule(`
+    style.sheet.insertRule(
+      `
       #mainBox::-webkit-scrollbar {
         width: 0px;
         height: 0px;
       }
-    `, style.sheet.cssRules.length);
+    `,
+      style.sheet.cssRules.length
+    );
   }
 }
 
